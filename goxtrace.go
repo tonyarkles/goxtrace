d607308c1b9f1fdc52fa1776e7c5c50eddc08a49
Tony Arkles <tony@peakxp.com>
2013-09-02 21:55:20 -0600

    Binding to a socket and doing something really simple

--- goxtrace.go
+++ goxtrace.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
+func handleConnection(conn net.Conn) {
+	conn.Write([]byte("Hello world!\n"))
+	conn.Close()
+}
+
+func runServer(binding string) {
+	ln, err := net.Listen("tcp", binding)
+	if err != nil {
+
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
 func main() {
-	fmt.Printf("Hello world!\n")
+	binding := ":4444"
+	fmt.Printf("Starting server on %s\n", binding)
+	runServer(binding)
 }
